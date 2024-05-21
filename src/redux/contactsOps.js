56263f491a69e1dbc90d8e2088d7c3fc343addb7
RomanYurkiv <yurkiv.r.official@gmail.com>
2024-05-21 11:31:29 +0300

    add api

--- src/redux/contactsOps.js
+++ src/redux/contactsOps.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-axios.defaults.baseURL = "https://6617f5b39a41b1b3dfbbd2b7.mockapi.io";
+axios.defaults.baseURL = "https://664c59de35bbda10987ffe92.mockapi.io";
 
 const fetchContacts = createAsyncThunk(
   "contacts/fetchAll",
   async (_, thunkAPI) => {
     try {
-      const response = await axios.get("/contacts");
+      const response = await axios.get("/Contacts");
       return response.data;
     } catch (e) {
       return thunkAPI.rejectWithValue(e.message);
